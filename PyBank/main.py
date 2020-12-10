3e8099b5673197e8f9049927d78aac3f131d13be
Xiaoran Li <74216423+xiaoran-w-li@users.noreply.github.com>
2020-12-10 00:25:11 -0500

    works up to total, also note, need to run with open every time you do a function on the csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,17 +7,26 @@ print ("----------------------------------------------")
 # Set path for file
 bank_data_csv = os.path.join("Resources", "budget_data.csv")
 
+
 # Open the CSV
 with open(bank_data_csv) as datafile:
-    datafilereader = csv.reader(datafile, delimiter=",")
-
+    datafile_reader = csv.reader(datafile, delimiter=",")
+ 
     # Get rid of headers
-    next(datafile)
+    next(datafile_reader)
 
     # Find number of months in total 
-    numline = len(datafile.readlines())
+    numline = len(list(datafile_reader))
     print ("Total Months:", (numline))
 
-
+with open(bank_data_csv) as datafile:
+    datafile_reader = csv.reader(datafile, delimiter=",")
+    next(datafile_reader)
+    # Calculate net total amount of "Profit/Losses"
+    total = 0
+    for row in datafile_reader:
+       total += float(row[1])
+    print ("Total: $"+str((total)))
+    
 
 
