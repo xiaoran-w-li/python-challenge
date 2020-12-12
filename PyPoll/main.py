3b244c865da2da2fe9f91993286b9f6ce9e07cc8
Xiaoran Li <74216423+xiaoran-w-li@users.noreply.github.com>
2020-12-12 18:03:09 -0500

    successfully finds number of votes and creates list of candidates running

---
+++ PyPoll/main.py
@@ -0,0 +1,42 @@
+import os
+import csv
+
+print ("Election Results")
+print ("----------------------------------------------")
+
+# Set path for file
+poll_data_csv = os.path.join("Resources", "election_data.csv")
+
+# Open the CSV, find number of votes
+with open(poll_data_csv) as datafile:
+    datafile_reader = csv.reader(datafile, delimiter=",")
+
+    # Get rid of headers
+    next(datafile_reader)
+
+    # Total number of votes cast
+    total_votes = len(list(datafile_reader))
+    print ("Total Votes:", (total_votes))
+
+print ("----------------------------------------------")
+
+# Open the CSV, find number of candidates
+
+# Create list for full candidate name 
+candidate_name = []
+
+
+with open(poll_data_csv) as datafile:
+    datafile_reader = csv.reader(datafile, delimiter=",")
+
+    # Skip Header
+    fields = next(datafile_reader)
+
+    # Populate candidate_name list 
+    for row in datafile_reader:
+        candidate_name.append(row[2])
+    
+    # Use 'set' to pull unique candidate names
+    unique_candidate = set(candidate_name)
+    unique_candidate = list(unique_candidate)
+
