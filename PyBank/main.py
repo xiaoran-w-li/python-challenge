9a4a5186f9819a2329524d52032567f565f9a5d6
Xiaoran Li <74216423+xiaoran-w-li@users.noreply.github.com>
2020-12-12 02:43:56 -0500

    I think it all works!

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,14 +19,52 @@ with open(bank_data_csv) as datafile:
     numline = len(list(datafile_reader))
     print ("Total Months:", (numline))
 
+# Calculate net total amount of "Profit/Losses"
 with open(bank_data_csv) as datafile:
     datafile_reader = csv.reader(datafile, delimiter=",")
     next(datafile_reader)
-    # Calculate net total amount of "Profit/Losses"
+
     total = 0
     for row in datafile_reader:
        total += float(row[1])
     print ("Total: $"+str((total)))
+
+
+#Calculate average profit/losses and max proft and max loss
+with open(bank_data_csv) as datafile:
+    datafile_reader = csv.reader(datafile, delimiter=",")
+    next(datafile_reader)
+
+    #list to store revenue data
+    revenue = []
+    #list of average change in revenue 
+    change_revenue = []
+    #list of dates
+    date = []
+
+
+    #populate list with second column [1] profit losses
+    #populate list with first column [0] date
+    for row in datafile_reader:
+        revenue.append(float(row[1]))
+        date.append(row[0])
     
+    #loop to find revenue change in newly populated revenue list
+    for i in range(1,len(revenue)):
+        change_revenue.append(revenue[i] - revenue[i-1])
+        average_change_revenue = sum(change_revenue)/len(change_revenue)
+
+        max_change_revenue = max(change_revenue)
+
+        min_change_revenue = min(change_revenue)
+
+        date_max_change_revenue = str(date[change_revenue.index(max(change_revenue))])
+        date_min_change_revenue = str(date[change_revenue.index(min(change_revenue))])
+
+    print ("Average Change: $"+str(average_change_revenue))
+    print ("Greatest Increase in Profits:", (date_max_change_revenue), "$"+str(max_change_revenue))
+    print ("Greatest Decrease in Profits:", (date_min_change_revenue), "$"+str(min_change_revenue))
+    print (date_max_change_revenue)
+    print (date_min_change_revenue)
 
 
